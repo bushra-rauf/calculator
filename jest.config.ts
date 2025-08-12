433e2c26bad00d192a05bee142dbf32fee3f1a9a
bushra-rauf <bush_rauf@yahoo.com>
2025-08-12 03:19:45 +0200

    basic structure

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+
+import type {Config} from 'jest';
+import nextJest from 'next/jest.js'
+ 
+const createJestConfig = nextJest({
+  dir: './',
+  })
+
+const config: Config = {
+    clearMocks: true,
+    coverageProvider: "v8",
+    testEnvironment: "jsdom",
+    setupFilesAfterEnv:["<rootDiv/jest.setup.ts>"]
+}
+
+  export default createJestConfig(config)
